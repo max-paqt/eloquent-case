857eb4f888f84018232ba4fff9c03f54016f5cf0
max-paqt <max.stekelenburg@paqt.com>
2025-07-17 12:56:03 +0200

    Add eloquent case

---
+++ Tests/EscapedCaseTest.php
@@ -0,0 +1,135 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Rxak\EloquentCase;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use Rxak\EloquentCase\EloquentCase;
+
+class EscapedCaseTest extends TestCase
+{
+    #[Test]
+    public function it_constructs_case_statements_with_1_case_and_else()
+    {
+        $case = (new EloquentCase())
+            ->when('age', '>', 18, 'adult')
+            ->else('minor');
+
+        $this->assertEquals([
+            'CASE WHEN `age` > ? THEN ? ELSE ?',
+            [18, 'adult', 'minor'],
+        ], $case->toArgs());
+    }
+
+    #[Test]
+    public function it_adds_as()
+    {
+        $case = (new EloquentCase())
+            ->when('age', '>', 18, 'adult')
+            ->else('minor')
+            ->as('age_group');
+
+        $this->assertEquals([
+            'CASE WHEN `age` > ? THEN ? ELSE ? AS `age_group`',
+            [18, 'adult', 'minor'],
+        ], $case->toArgs());
+    }
+
+    #[Test]
+    public function it_constructs_case_statements_with_multiple_whens_and_else()
+    {
+        $case = (new EloquentCase())
+            ->when('gender', '=', 'm', 'señor')
+            ->when('age', '>', 65, 'senior')
+            ->when('age', '>', 40, 'adult')
+            ->when('age', '>', 18, 'young adult')
+            ->else('minor');
+
+        $this->assertEquals([
+            'CASE WHEN `gender` = ? THEN ? '
+            . 'WHEN `age` > ? THEN ? '
+            . 'WHEN `age` > ? THEN ? '
+            . 'WHEN `age` > ? THEN ? '
+            . 'ELSE ?',
+            ['m', 'señor', 65, 'senior', 40, 'adult', 18, 'young adult', 'minor'],
+        ], $case->toArgs());
+    }
+
+    #[Test]
+    public function it_constructs_case_statements_with_1_case_without_else()
+    {
+        $case = (new EloquentCase())
+            ->when('age', '>', 18, 'adult');
+
+        $this->assertEquals([
+            'CASE WHEN `age` > ? THEN ?',
+            [18, 'adult'],
+        ], $case->toArgs());
+    }
+
+    #[Test]
+    public function it_constructs_case_statements_with_multiple_whens_without_else()
+    {
+        $case = (new EloquentCase())
+            ->when('gender', '=', 'm', 'señor')
+            ->when('age', '>', 65, 'senior')
+            ->when('age', '>', 40, 'adult')
+            ->when('age', '>', 18, 'young adult');
+
+        $this->assertEquals([
+            'CASE WHEN `gender` = ? THEN ? '
+            . 'WHEN `age` > ? THEN ? '
+            . 'WHEN `age` > ? THEN ? '
+            . 'WHEN `age` > ? THEN ?',
+            ['m', 'señor', 65, 'senior', 40, 'adult', 18, 'young adult'],
+        ], $case->toArgs());
+    }
+
+    #[Test]
+    public function it_uses_enum_values_for_backed_enums()
+    {
+        $case = (new EloquentCase())
+            ->when('my_column', '=', MyEnum::First, MyEnum::Second)
+            ->else(MyEnum::Third);
+
+        $this->assertEquals([
+            'CASE WHEN `my_column` = ? THEN ? ELSE ?',
+            [MyEnum::First->value, MyEnum::Second->value, MyEnum::Third->value],
+        ], $case->toArgs());
+    }
+
+    #[Test]
+    public function it_allows_raw_when()
+    {
+        $case = (new EloquentCase())
+            ->whenRaw('my risky wen', 'value')
+            ->else('else');
+
+        $this->assertEquals([
+            'CASE WHEN my risky wen THEN ? ELSE ?',
+            ['value', 'else'],
+        ], $case->toArgs());
+    }
+
+    #[Test]
+    public function it_allows_raw_when_with_enum()
+    {
+        $case = (new EloquentCase())
+            ->whenRaw('my risky wen', MyEnum::First)
+            ->else(MyEnum::Second);
+
+        $this->assertEquals([
+            'CASE WHEN my risky wen THEN ? ELSE ?',
+            [MyEnum::First->value, MyEnum::Second->value],
+        ], $case->toArgs());
+    }
+}
+
+enum MyEnum: string
+{
+    case First = 'first';
+    case Second = 'second';
+    case Third = 'third';
+}
---
+++ src/EloquentCase.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rxak\EloquentCase;
+
+use BackedEnum;
+
+class EloquentCase
+{
+    /** @var string[] */
+    private array $when = [];
+
+    /** @var mixed[] */
+    private array $bindings = [];
+
+    private mixed $else;
+
+    private string $as;
+
+    public function whenRaw(string $raw, mixed $then): static
+    {
+        $this->when[] = sprintf("WHEN %s THEN ?", $raw);
+        $this->bindings[] = $this->getValue($then);
+
+        return $this;
+    }
+
+    public function when(string $column, string $operator, mixed $value, mixed $then): static
+    {
+        $this->when[] = sprintf('WHEN `%s` %s ? THEN ?', $column, $operator);
+        $this->bindings[] = $this->getValue($value);
+        $this->bindings[] = $this->getValue($then);
+
+        return $this;
+    }
+
+    private function getValue(mixed $value): mixed
+    {
+        return $value instanceof BackedEnum ? $value->value : $value;
+    }
+
+    public function else(mixed $then): static
+    {
+        $this->else = $then;
+
+        return $this;
+    }
+
+    public function as(string $as): static
+    {
+        $this->as = $as;
+
+        return $this;
+    }
+
+    public function toArgs(): array
+    {
+        $statement = 'CASE ' . implode(' ', $this->when);
+        $bindings = $this->bindings;
+
+        if (isset($this->else)) {
+            $statement .= ' ELSE ?';
+            $bindings[] = $this->getValue($this->else);
+        }
+
+        if (isset($this->as)) {
+            $statement .= sprintf(' AS `%s`', $this->as);
+        }
+
+        return [$statement, $bindings];
+    }
+}
